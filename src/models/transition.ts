9c1fdba511538d3b9ac65a831de74e08a9a0d316
Igordeveloperr <g.bikmetow@gmail.com>
2025-06-30 17:42:38 +0300

    upd: добавил описание полей

--- src/models/transition.ts
+++ src/models/transition.ts
@@ -2,10 +2,18 @@ import { z } from "zod";
 import { defaultTypeSchema } from "./baseSchemas";
 
 const transitionSchema = z.object({
-  id: z.string(),
-  self: z.string().optional(),
-  display: z.string().optional(),
-  to: defaultTypeSchema,
+  id: z.string().describe("Идентификатор перехода"),
+  self: z
+    .string()
+    .optional()
+    .describe("Адрес ресурса API, который содержит информацию о переходе"),
+  display: z
+    .string()
+    .optional()
+    .describe(
+      "Отображаемое имя перехода. Соответствует названию кнопки в интерфейсе Трекера"
+    ),
+  to: defaultTypeSchema.describe("Объект с информацией о новом статусе задачи"),
 });
 
 export type TransitionType = z.infer<typeof transitionSchema>;
