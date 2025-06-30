c262701363356374169d822a035f3ae25f8845d6
Igordeveloperr <g.bikmetow@gmail.com>
2025-06-30 18:18:45 +0300

    crt: описал модель записи из истории изменений задачи

---
+++ src/models/changelogItem.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+import { defaultTypeSchema, versionSchema } from "./baseSchemas";
+import { userSchemaSimple } from "./user";
+
+export const changelogItemSchema = z.object({
+  id: z.string(),
+  self: z.string().optional(),
+  issue: defaultTypeSchema,
+  updatedAt: z.string().optional(),
+  updatedBy: userSchemaSimple.optional(),
+  type: z.string().optional(),
+  fields: z.array(z.object({
+    field: versionSchema.optional(),
+    from: defaultTypeSchema.optional(),
+    to: defaultTypeSchema.optional()
+  })).optional(),
+});
