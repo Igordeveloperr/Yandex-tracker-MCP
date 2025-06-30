202b68faae47d8e65c3c74cb92138b7365bcfd2f
Igordeveloperr <g.bikmetow@gmail.com>
2025-06-30 19:19:44 +0300

    crt: описал модель чеклиста

---
+++ src/models/checkList.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+export const checkListSchema = z.object({
+  id: z.string(),
+  text: z.string().optional(),
+  textHtml: z.string().optional(),
+  checked: z.boolean().optional(),
+  assignee: z
+    .object({
+      id: z.number(),
+      display: z.string().optional(),
+    })
+    .optional(),
+  deadline: z
+    .object({
+      date: z.string().optional(),
+      deadlineType: z.string().optional(),
+      isExceeded: z.boolean().optional(),
+    })
+    .optional(),
+});
+
+export type CheckListType = z.infer<typeof checkListSchema>;
