71b124f1944e9a53d58ddf311bf667acc5481ad8
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-04 10:51:09 +0300

    ref: убрал чтение файлов и импортировал ресурсы по queue

--- src/mcp/YandexTrackerMcpServer.ts
+++ src/mcp/YandexTrackerMcpServer.ts
@@ -20,7 +20,7 @@ import { CheckListType } from "../models/issues/checklist";
 import { ChangelogItemType } from "../models/issues/changelogItem";
 import { TransitionType } from "../models/issues/transition";
 import { isAxiosError } from "axios";
-import { howToUseQuery, queryParametersDoc, userFieldsDoc } from "../models/models_descrides/resource";
+import { howToUseQuery, queryParametersDoc, queueFieldsDoc, userFieldsDoc } from "../models/models_descrides/resource";
 
 export class YandexTrackerMcpServer extends YandexMcpServer {
   /**
@@ -485,13 +485,7 @@ export class YandexTrackerMcpServer extends YandexMcpServer {
     extra: RequestHandlerExtra<ServerRequest, ServerNotification>
   ): Promise<CallToolResult> {
     try {
-      // Чтение файла queue.json
-      const issueFields = await fs.readFile(
-        `${config.MODEL_DESCRIPTION_BASE_PATH}${ModelDescriptionName.queue}`,
-        "utf-8"
-      );
-      const issueFieldsData: object[] = await JSON.parse(issueFields);
-      return super.receiveCallToolResult<object[]>(issueFieldsData);
+      return super.receiveCallToolResult<object[]>(queueFieldsDoc);
     } catch (error) {
       throw error;
     }
