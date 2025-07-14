28f9847ef703d40b767f3ec714afb48ba5ff90d2
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-14 11:30:44 +0300

    ref: убрал лишние импорты

--- src/yandex_api/YandexTrackerReadAPI.ts
+++ src/yandex_api/YandexTrackerReadAPI.ts
@@ -1,12 +1,8 @@
-import { logger } from "../settings/logger";
 import {
   Issue,
   issueSchema,
-  SimpleIssue,
   issueSchemaSimple,
 } from "../models/issues/issue";
-import { config } from "../settings/config";
-import { Tracker } from "yandex-tracker-client";
 import { ExpandQueue, Queue, queueSchema } from "../models/queues/queue";
 import { userSchemaSimple, SimpleUser, userSchema, User } from "../models/users/user";
 import {
@@ -16,9 +12,7 @@ import {
   issueTypeSchema,
   Status,
   statusSchema,
-  defaultTypeSchema,
 } from "../models/baseSchemas";
-import { response } from "express";
 import { transitionSchema, TransitionType } from "../models/issues/transition";
 import { changelogItemSchema, ChangelogItemType } from "../models/issues/changelogItem";
 import { checkListSchema, CheckListType } from "../models/issues/checklist";
