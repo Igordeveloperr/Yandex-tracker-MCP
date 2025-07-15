eacc0c3b81c5b696a254c1a0cebe4f802adadf85
alexsoulless <onilove.sa@gmail.com>
2025-07-15 15:12:49 +0300

    crt: добавил модель новой доски

--- src/models/boards/board.ts
+++ src/models/boards/board.ts
@@ -1,8 +1,8 @@
 import z from "zod";
 
 export const boardSchema = z.object({
-    id: z.number(),
-    name: z.string().optional()
+  id: z.number(),
+  name: z.string().optional(),
 });
 export const extendsBoardSchema = boardSchema.extend({
   defaultQueue: z.object({
@@ -12,3 +12,21 @@ export const extendsBoardSchema = boardSchema.extend({
 });
 export type BoardType = z.infer<typeof boardSchema>;
 export type ExtendBoardType = z.infer<typeof extendsBoardSchema>;
+
+export const createBoardSchema = z.object({
+  name: z.string(),
+  defaultQueue: z.object({
+    id: z.string().optional(),
+    key: z.string(),
+  }),
+  boardType: z.enum(["default", "scrum", "kanban"]).optional(),
+  query: z.string().optional(),
+  useRanking: z.boolean().optional(),
+  country: z
+    .object({
+      id: z.string(),
+    })
+    .optional(),
+});
+
+export type CreateBoard = z.infer<typeof createBoardSchema>;
