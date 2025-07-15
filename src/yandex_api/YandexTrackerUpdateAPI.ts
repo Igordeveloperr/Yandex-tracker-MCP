259ecbfa1c71104b6e946d32e45cadef4e090583
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-15 11:47:08 +0300

    ref: форматирование

--- src/yandex_api/YandexTrackerUpdateAPI.ts
+++ src/yandex_api/YandexTrackerUpdateAPI.ts
@@ -2,24 +2,25 @@ import { logger } from "../settings/logger";
 import { IYandexTrackerUpdateAPI } from "./interfaces/IYandexTrackerUpdateAPI";
 import { YandexTrackerAPI } from "./YandexTrackerAPI";
 
-export class YandexTrackerUpdateAPI extends YandexTrackerAPI implements IYandexTrackerUpdateAPI{
+export class YandexTrackerUpdateAPI
+  extends YandexTrackerAPI
+  implements IYandexTrackerUpdateAPI
+{
   private static _instance: YandexTrackerUpdateAPI;
   private constructor() {
     super();
   }
 
-    public static getInstance(): YandexTrackerUpdateAPI {
-      if (!YandexTrackerUpdateAPI._instance) {
-        try {
-          logger.debug("Создание экземпляра YandexTrackerAPI");
-          YandexTrackerUpdateAPI._instance = new YandexTrackerUpdateAPI();
-        } catch (error) {
-          logger.error("Не удалось создать экземпляр YandexTrackerAPI");
-          throw error;
-        }
+  public static getInstance(): YandexTrackerUpdateAPI {
+    if (!YandexTrackerUpdateAPI._instance) {
+      try {
+        logger.debug("Создание экземпляра YandexTrackerAPI");
+        YandexTrackerUpdateAPI._instance = new YandexTrackerUpdateAPI();
+      } catch (error) {
+        logger.error("Не удалось создать экземпляр YandexTrackerAPI");
+        throw error;
       }
-      return YandexTrackerUpdateAPI._instance;
     }
-
-    
+    return YandexTrackerUpdateAPI._instance;
+  }
 }
