b47a5cd2215c4a3da7100f261ea703102bf7ed55
alexsoulless <onilove.sa@gmail.com>
2025-07-15 15:17:58 +0300

    crt: создал модель нового спринта

--- src/models/boards/sprint.ts
+++ src/models/boards/sprint.ts
@@ -22,3 +22,14 @@ export const sprintSchema = z.object({
 });
 
 export type SprintType = z.infer<typeof sprintSchema>;
+
+export const createSprintSchema = z.object({
+  name: z.string(),
+  board: z.object({
+    id: z.string(),
+  }),
+  startDate: z.string(), // формат YYYY-MM-DD
+  endDate: z.string(),   // формат YYYY-MM-DD
+});
+
+export type CreateSprint = z.infer<typeof createSprintSchema>;
