78db02837f23e89a0092d81a68c0693c0e41ed8f
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-15 12:46:55 +0300

    upd: описал объекты параметров для регистрации промптов и отрефакторил регистрацию промптов

--- src/mcp/YandexTrackerMcpServer.ts
+++ src/mcp/YandexTrackerMcpServer.ts
@@ -18,6 +18,7 @@ import { ChangelogItemType } from "../models/issues/changelogItem";
 import { TransitionType } from "../models/issues/transition";
 import { isAxiosError } from "axios";
 import { howToUseQuery, issueFieldsDoc, queryParametersDoc, queueFieldsDoc, userFieldsDoc } from "../models/resource";
+import { searchIssuePromptParam, taskSummaryPromptParam } from "../models/mcp_params/promptParams";
 
 export class YandexTrackerMcpServer extends YandexMcpServer {
   /**
@@ -33,28 +34,16 @@ export class YandexTrackerMcpServer extends YandexMcpServer {
   // регистрируем все MCP prompts связанные с Yandex Tracker
   protected addPrompts(): void {
     this.mcpServer.prompt(
-      YandexTrackerPromptName.taskSummary,
-      "Краткое изложение задачи: суть задачи, статус, приоритет, исполнитель",
-      { issueKey: z.string().describe("Ключ задачи") },
+      taskSummaryPromptParam.name,
+      taskSummaryPromptParam.systemPrompt,
+      taskSummaryPromptParam.argsSchema,
       this.getTaskSummaryPromptCallBack.bind(this)
     );
 
     this.mcpServer.prompt(
-      YandexTrackerPromptName.searchIssue,
-      "Поиск задач по основным полям",
-      {
-        issueCount: z
-          .string()
-          .regex(/^[1-9]\d*$/, {
-            message: "Должно быть целое число больше нуля",
-          })
-          .describe("Кол-во задач"),
-        queueKey: z.string().describe("Ключ очереди"),
-        status: z.string().describe("Статус задачи"),
-        priority: z.string().describe("Приоритет задачи"),
-        issueType: z.string().describe("Тип задачи"),
-        name: z.string().describe("Имя и Фамилия исполнителя"),
-      },
+      searchIssuePromptParam.name,
+      searchIssuePromptParam.systemPrompt,
+      searchIssuePromptParam.argsSchema,
       this.searchIssuePromptCallBack.bind(this)
     );
   }
