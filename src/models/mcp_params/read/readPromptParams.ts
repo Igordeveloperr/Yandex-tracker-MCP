467a732c8f20889f0b65205379d66f60561762d6
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-17 09:35:09 +0300

    ref: rename

--- src/models/mcp_params/read/readPromptParams.ts
+++ src/models/mcp_params/read/readPromptParams.ts
@@ -1,33 +1,33 @@
 import z from "zod";
 import { YandexTrackerPromptName } from "../../../enums/YandexTrackerPromptName";
 import { IParamConfig } from "../../../mcp/IParamConfig";
+import { IReadPromptCallback } from "../../../mcp/callback/read/IReadPromptCallback";
 
-export const readPromptArray = [];
-// export const promptArray: IParamConfig[] = [
-//   {
-//     name: YandexTrackerPromptName.taskSummary,
-//     systemPrompt:
-//       "Краткое изложение задачи: суть задачи, статус, приоритет, исполнитель",
-//     paramsSchema: { issueKey: z.string().describe("Ключ задачи") },
-//     callbackKey: "getTaskSummaryPromptCallBack",
-//   },
+export const readPromptArray: IParamConfig<IReadPromptCallback>[] = [
+  {
+    name: YandexTrackerPromptName.taskSummary,
+    systemPrompt:
+      "Краткое изложение задачи: суть задачи, статус, приоритет, исполнитель",
+    paramsSchema: { issueKey: z.string().describe("Ключ задачи") },
+    callbackKey: "getTaskSummaryPromptCallBack",
+  },
 
-//   {
-//     name: YandexTrackerPromptName.searchIssue,
-//     systemPrompt: "Поиск задач по основным полям",
-//     paramsSchema: {
-//       issueCount: z
-//         .string()
-//         .regex(/^[1-9]\d*$/, {
-//           message: "Должно быть целое число больше нуля",
-//         })
-//         .describe("Кол-во задач"),
-//       queueKey: z.string().describe("Ключ очереди"),
-//       status: z.string().describe("Статус задачи"),
-//       priority: z.string().describe("Приоритет задачи"),
-//       issueType: z.string().describe("Тип задачи"),
-//       name: z.string().describe("Имя и Фамилия исполнителя"),
-//     },
-//     callbackKey: "searchIssuePromptCallBack",
-//   },
-// ];
+  {
+    name: YandexTrackerPromptName.searchIssue,
+    systemPrompt: "Поиск задач по основным полям",
+    paramsSchema: {
+      issueCount: z
+        .string()
+        .regex(/^[1-9]\d*$/, {
+          message: "Должно быть целое число больше нуля",
+        })
+        .describe("Кол-во задач"),
+      queueKey: z.string().describe("Ключ очереди"),
+      status: z.string().describe("Статус задачи"),
+      priority: z.string().describe("Приоритет задачи"),
+      issueType: z.string().describe("Тип задачи"),
+      name: z.string().describe("Имя и Фамилия исполнителя"),
+    },
+    callbackKey: "searchIssuePromptCallBack",
+  },
+];
