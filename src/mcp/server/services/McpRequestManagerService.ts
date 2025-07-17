4a172ea3a1a401541b134199d045ed447e1c7e6a
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-17 12:07:52 +0300

    upd: описал класс по управлению запросами

--- src/mcp/server/services/McpRequestManagerService.ts
+++ src/mcp/server/services/McpRequestManagerService.ts
@@ -1,3 +1,61 @@
+import { CallToolResult, GetPromptResult } from "@modelcontextprotocol/sdk/types";
+
 export class McpRequestManagerService{
-    
+  // формирование ответа для tools
+  public static receiveCallToolResult<Type>(response: Type): CallToolResult {
+    try {
+      return {
+        content: [
+          {
+            type: "text",
+            text:
+              typeof response === "string"
+                ? response
+                : JSON.stringify(response),
+          },
+        ],
+      };
+    } catch (error) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Ошибка: ${
+              error instanceof Error ? error.message : "Неизвестная ошибка"
+            }`,
+          },
+        ],
+        isError: true,
+      };
+    }
+  }
+
+  // формирование ответа для promts
+  public static receivePromptResult(response: string): GetPromptResult {
+    try {
+      return {
+        messages: [
+          {
+            role: "user" as const, // Важно указать константный тип
+            content: {
+              type: "text" as const, // Тип контента - текст
+              text: response,
+            },
+          },
+        ],
+      };
+    } catch (error) {
+      return {
+        messages: [
+          {
+            role: "user" as const,
+            content: {
+              type: "text" as const,
+              text: "Ошибка выполнения промпта",
+            },
+          },
+        ],
+      };
+    }
+  }
 }
