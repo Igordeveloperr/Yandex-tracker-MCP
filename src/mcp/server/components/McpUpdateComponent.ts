5c045ac054e4d01f33af82315e7dbe844fd5fabf
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-17 12:00:21 +0300

    crt: компонент обновления данных

---
+++ src/mcp/server/components/McpUpdateComponent.ts
@@ -0,0 +1,14 @@
+import { IMcpComponent } from "./IMcpComponent";
+
+export class McpUpdateComponent implements IMcpComponent{
+    addTools(): void {
+        throw new Error("Method not implemented.");
+    }
+    addResources(): void {
+        throw new Error("Method not implemented.");
+    }
+    addPrompts(): void {
+        throw new Error("Method not implemented.");
+    }
+
+}
