ad60a9b71f6d95a9ff4c07e3c3cc845bcc508d4f
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-17 09:33:31 +0300

    upd: описал необходимые сигнатуры методов

--- src/mcp/callback/read/IReadPromptCallback.ts
+++ src/mcp/callback/read/IReadPromptCallback.ts
@@ -1,5 +1,22 @@
+import { RequestHandlerExtra } from "@modelcontextprotocol/sdk/shared/protocol";
 import { ICallback } from "../ICallback";
+import { GetPromptResult, ServerNotification, ServerRequest } from "@modelcontextprotocol/sdk/types";
 
-export interface IReadPromptCallback extends ICallback{
-    
+export interface IReadPromptCallback extends ICallback {
+  searchIssuePromptCallBack(
+    _args: {
+      issueCount: string;
+      queueKey: string;
+      status: string;
+      priority: string;
+      issueType: string;
+      name: string;
+    },
+    _extra: RequestHandlerExtra<ServerRequest, ServerNotification>
+  ): Promise<GetPromptResult>;
+
+  getTaskSummaryPromptCallBack(
+    _args: { issueKey: string },
+    _extra: RequestHandlerExtra<ServerRequest, ServerNotification>
+  ): Promise<GetPromptResult>;
 }
