54b4698629f1c254f32ea9142f15e43d4018faad
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-17 12:14:09 +0300

    upd: докинул базы

--- src/mcp/server/components/McpDeleteComponent.ts
+++ src/mcp/server/components/McpDeleteComponent.ts
@@ -1,6 +1,12 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import { IMcpComponent } from "./IMcpComponent";
 
 export class McpDeleteComponent implements IMcpComponent{
+    private _mcpServer: McpServer;
+    constructor(mcpServer:McpServer) {
+        this._mcpServer = mcpServer;
+    }
+    
     addTools(): void {
         
     }
--- src/mcp/server/components/McpWriteComponent.ts
+++ src/mcp/server/components/McpWriteComponent.ts
@@ -1,6 +1,12 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import { IMcpComponent } from "./IMcpComponent";
 
 export class McpWriteComponent implements IMcpComponent{
+    private _mcpServer: McpServer;
+    constructor(mcpServer:McpServer) {
+        this._mcpServer = mcpServer;
+    }
+    
     addTools(): void {
         
     }
