d283f71c4fa7aa4f9cb4586b39a52b65b288771b
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-17 12:03:05 +0300

    ref: удалил мусор

--- src/mcp/server/components/McpDeleteComponent.ts
+++ src/mcp/server/components/McpDeleteComponent.ts
@@ -2,13 +2,13 @@ import { IMcpComponent } from "./IMcpComponent";
 
 export class McpDeleteComponent implements IMcpComponent{
     addTools(): void {
-        throw new Error("Method not implemented.");
+        
     }
     addResources(): void {
-        throw new Error("Method not implemented.");
+        
     }
     addPrompts(): void {
-        throw new Error("Method not implemented.");
+        
     }
 
 }
--- src/mcp/server/components/McpWriteComponent.ts
+++ src/mcp/server/components/McpWriteComponent.ts
@@ -2,13 +2,13 @@ import { IMcpComponent } from "./IMcpComponent";
 
 export class McpWriteComponent implements IMcpComponent{
     addTools(): void {
-        throw new Error("Method not implemented.");
+        
     }
     addResources(): void {
-        throw new Error("Method not implemented.");
+        
     }
     addPrompts(): void {
-        throw new Error("Method not implemented.");
+        
     }
 
 }
