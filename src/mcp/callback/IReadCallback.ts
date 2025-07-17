863d9cc1e58e24b753caff08bbf99c4fc91211c2
Igordeveloperr <g.bikmetow@gmail.com>
2025-07-17 09:10:10 +0300

    upd: описал сигнатуру

--- src/mcp/callback/IReadCallback.ts
+++ src/mcp/callback/IReadCallback.ts
@@ -1,5 +1,12 @@
+import z from "zod";
 import { ICallback } from "./ICallback";
+import { getIssueDefaultParamSchema } from "../../models/paramShemas";
+import { RequestHandlerExtra } from "@modelcontextprotocol/sdk/shared/protocol";
+import { CallToolResult, ServerNotification, ServerRequest } from "@modelcontextprotocol/sdk/types";
 
 export interface IReadCallback extends ICallback{
-    
+  getIssueTransitionsToolCallback(
+    args: z.infer<typeof getIssueDefaultParamSchema>,
+    extra: RequestHandlerExtra<ServerRequest, ServerNotification>
+  ): Promise<CallToolResult>;
 }
